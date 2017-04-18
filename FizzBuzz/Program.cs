458b891c6a5a228465be09a0db486250abfd275a
Scott Pantall <aspantall@gmail.com>
2017-04-18 16:47:12 -0600

    writes to console

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,8 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("HEY YOU GUUYYYS!");
+            Console.ReadKey();
         }
     }
 }
