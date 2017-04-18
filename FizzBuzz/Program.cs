af4d800a33d788c867870e5de6db5a0f3d808d5e
Scott Pantall <aspantall@gmail.com>
2017-04-18 16:58:25 -0600

    Reads file and writes to console

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,16 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             Console.WriteLine("HEY YOU GUUYYYS!");
-            Console.ReadKey();
+
+            using (StreamReader reader = File.OpenText(args[0]))
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(); //Pauses app until key is pressed
         }
     }
 }
